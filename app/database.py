dd83651a96ba2cf7859b8c398996d539b3cb20c9
Ahmed <ahmedfather555@gmail.com>
2026-04-18 00:08:59 +0200

    stuff for the server

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:admin@localhost:5432/arabity_db"
+DATABASE_URL = "postgresql://arabblbo_arabity_db:JFd7ZxtB_oNQ@localhost:5432/arabblbo_arabity_db"
 # DATABASE_URL=  "postgresql://postgres:admin@db:5432/arabity_db"
 
 engine = create_engine(DATABASE_URL)
